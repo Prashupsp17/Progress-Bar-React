06292a614166e9efa4ad935e56486b136173d66e
Prashupsp17 <shindeprashant967@gmail.com>
2023-12-10 19:21:55 +0530

    Progress Bar using React

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import ProgressBar from './components/ProgressBar';
+import { useEffect, useState } from 'react';
+
+function App() {
+
+  const [value, setValue] = useState(0);
+  const [success, setSuccess] = useState(false);
+
+  useEffect(() => {
+    setInterval(() => {
+      setValue((val) => val + 1);
+    }, 100);
+  }, []);
+
+  return (
+    <div className="App">
+      <span>Progress Bar</span>
+      <ProgressBar
+        value={value}
+        onComplete={() => setSuccess(true)} />
+      <span>{success ? "Complete" : "Loading..."}</span>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ProgressBar.js
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from 'react'
+
+const ProgressBar = ({ value = 0, onComplete = () => { } }) => {
+    const [percent, setPercent] = useState(value);
+
+    useEffect(() => {
+        setPercent(Math.min(100, Math.max(value, 0)));
+
+        if (value >= 100) {
+            onComplete();
+        }
+    }, [value]);
+
+    return (
+        <div className="progress">
+            <span style={{ color: percent > 49 ? "white" : "black" }}>{percent.toFixed()}%</span>
+            <div style={{ width: `${percent}%` }} />
+        </div>
+    )
+}
+
+export default ProgressBar
